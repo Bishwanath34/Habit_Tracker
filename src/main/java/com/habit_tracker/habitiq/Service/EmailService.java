941893517c202f3d2b92cf0762a4882df581bc67
Bishwanath34 <91818190+Bishwanath34@users.noreply.github.com>
2026-03-10 09:56:03 +0530

    Refactor EmailService to use configurable sender email

--- src/main/java/com/habit_tracker/habitiq/Service/EmailService.java
+++ src/main/java/com/habit_tracker/habitiq/Service/EmailService.java
@@ -3,6 +3,7 @@ package com.habit_tracker.habitiq.Service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,16 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.from}")
+    private String mailFrom;
+
     // ================= SEND OTP EMAIL =================
     public void sendOtpEmail(String toEmail, String otp) throws MessagingException {
 
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
+        helper.setFrom(mailFrom);
         helper.setTo(toEmail);
         helper.setSubject("HabitIQ – Email Verification OTP");
 
@@ -26,9 +31,7 @@ public class EmailService {
                 <div style="font-family:Arial,sans-serif">
                     <h2>Welcome to HabitIQ</h2>
                     <p>Please verify your email using the OTP below:</p>
-                """
-                + "<h1 style=\"color:#4CAF50;\">" + otp + "</h1>"
-                + """
+                     <h1 style="color:#4CAF50;">""" + otp + """</h1>
                     <p>This OTP will expire in 5 minutes.</p>
                     <br>
                     <p>Thank you for using HabitIQ 🚀</p>
@@ -41,19 +44,19 @@ public class EmailService {
     }
 
     // ================= SEND REMINDER EMAIL =================
-    public void sendReminderEmail(String toEmail) throws MessagingException {
+    public void sendReminderEmail(String toEmail, String messageText) throws MessagingException {
 
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
+        helper.setFrom(mailFrom);
         helper.setTo(toEmail);
         helper.setSubject("HabitIQ Reminder ⏰");
 
         String content = """
                 <div style="font-family:Arial,sans-serif">
                     <h2>⏰ Habit Reminder</h2>
-                    <p>Don't forget to complete your habits today!</p>
-                    <p>Consistency builds success 💪</p>
+                    <p>""" + messageText + """</p>
                     <br>
                     <p>Open HabitIQ and track your progress.</p>
                 </div>
